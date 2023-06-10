9cd63559d889dfc07277c3704db5da0b5528ae9a
BeccaQ <72604781+Beccaish@users.noreply.github.com>
2023-06-10 21:41:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -226,3 +226,11 @@ fsEnter.addEventListener('click', function (e) {
         fsEnter.innerHTML = "Go Fullscreen";
     }
 });
+
+
+var angle = 0;
+function galleryspin(sign) { 
+spinner = document.querySelector("#spinner");
+if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+}
